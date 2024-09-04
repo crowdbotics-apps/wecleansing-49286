2c0f56e914a37aae645f8bf3528a5330aeb9640a
Crowdbotics <engineering@crowdbotics.com>
2024-09-04 23:05:38 +0000

    Update store.

--- store/wecleansingAPI/api.js
+++ store/wecleansingAPI/api.js
@@ -14,6 +14,52 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return wecleansingAPI.post(`/api/v1/signup/`, payload)
 }
+function modules_disqus_comment_list(payload) {
+  return wecleansingAPI.get(`/modules/disqus/comment/`, {
+    params: { page: payload.page, page_size: payload.page_size }
+  })
+}
+function modules_disqus_comment_create(payload) {
+  return wecleansingAPI.post(`/modules/disqus/comment/`, payload)
+}
+function modules_disqus_comment_like_list(payload) {
+  return wecleansingAPI.get(`/modules/disqus/comment-like/`, {
+    params: { page: payload.page, page_size: payload.page_size }
+  })
+}
+function modules_disqus_comment_like_create(payload) {
+  return wecleansingAPI.post(`/modules/disqus/comment-like/`, payload)
+}
+function modules_disqus_comment_like_retrieve(payload) {
+  return wecleansingAPI.get(`/modules/disqus/comment-like/${payload.id}/`)
+}
+function modules_disqus_comment_like_update(payload) {
+  return wecleansingAPI.put(
+    `/modules/disqus/comment-like/${payload.id}/`,
+    payload
+  )
+}
+function modules_disqus_comment_like_partial_update(payload) {
+  return wecleansingAPI.patch(
+    `/modules/disqus/comment-like/${payload.id}/`,
+    payload
+  )
+}
+function modules_disqus_comment_like_destroy(payload) {
+  return wecleansingAPI.delete(`/modules/disqus/comment-like/${payload.id}/`)
+}
+function modules_disqus_comment_retrieve(payload) {
+  return wecleansingAPI.get(`/modules/disqus/comment/${payload.id}/`)
+}
+function modules_disqus_comment_update(payload) {
+  return wecleansingAPI.put(`/modules/disqus/comment/${payload.id}/`, payload)
+}
+function modules_disqus_comment_partial_update(payload) {
+  return wecleansingAPI.patch(`/modules/disqus/comment/${payload.id}/`, payload)
+}
+function modules_disqus_comment_destroy(payload) {
+  return wecleansingAPI.delete(`/modules/disqus/comment/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
   return wecleansingAPI.post(`/rest-auth/login/`, payload)
 }
@@ -51,6 +97,18 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_disqus_comment_list,
+  modules_disqus_comment_create,
+  modules_disqus_comment_like_list,
+  modules_disqus_comment_like_create,
+  modules_disqus_comment_like_retrieve,
+  modules_disqus_comment_like_update,
+  modules_disqus_comment_like_partial_update,
+  modules_disqus_comment_like_destroy,
+  modules_disqus_comment_retrieve,
+  modules_disqus_comment_update,
+  modules_disqus_comment_partial_update,
+  modules_disqus_comment_destroy,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
